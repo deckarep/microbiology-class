633bc5e2402413532db5b683d02b62d050b6ce2e
Ralph Caraveo <deckarep@gmail.com>
2019-08-21 11:32:47 -0700

    first commit

---
+++ week1/data/slide46.js
@@ -0,0 +1,11 @@
+(function(){var loadHandler=window['sl_{809E3923-D2AC-4FCD-8FBC-D2FF58064AE0}'];loadHandler&&loadHandler(45, '<div id="spr0_49a97e11"><div id="spr1_49a97e11" class="kern slide"><img id="img6_49a97e11" src="data/img11.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_49a97e11" class="kern slide"><div id="spr3_49a97e11" style="left:34px;top:33px;"><img id="img0_49a97e11" src="data/img22.png" width="123" height="28" alt="Lab 3-1 "/></div><div id="spr4_49a97e11" style="left:178px;top:33px;"><img id="img1_49a97e11" src="data/img25.png" width="496" height="31" alt="Introduction to the Microscope "/></div><div id="spr5_49a97e11" style="left:4px;top:65px;"><img id="img2_49a97e11" src="data/img120.png" width="720" height="76" alt="Using the Microscope!"/></div><div id="spr6_49a97e11" style="left:18px;top:136px;"><img id="img3_49a97e11" src="data/img121.png" width="665" height="32" alt="5 IMPORTANT RULES YOU MUST FOLLOW:"/></div><div id="spr7_49a97e11" style="left:12px;top:178px;"><img id="img4_49a97e11" src="data/img122.png" width="234" height="335" alt="Always start on the lowest objective (4x scanning).\
+\
+Don’t move up to a higher objective until you have focused on your sample under the lower objective.\
+\
+NEVER turn the coarse focus knob while your eyes are looking into the eyepiece. You will crack your sample and break your microscope 😬\
+\
+Oil can ONLY be used with the 100x objective!\
+\
+"/></div><div id="spr8_49a97e11" style="left:256px;top:451px;"><img id="img5_49a97e11" src="data/img123.png" width="452" height="77" alt="5. Always clean your microscope \
+    when you’re done for the day. We will \
+     know when you don’t, and you’ll lose points!"/></div><div id="vd0_49a97e11" style="left:252.715px;top:172.014px;"></div></div></div>');})();
