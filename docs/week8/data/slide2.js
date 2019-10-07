3e40c08bfdfdb7bb641770a8398aab28f46abe2a
Ralph Caraveo <deckarep@gmail.com>
2019-10-07 10:21:21 -0700

    week 8

---
+++ docs/week8/data/slide2.js
@@ -0,0 +1,12 @@
+(function(){var loadHandler=window['sl_{DBD071E0-9207-4A71-B374-2AEF8BEF9AFA}'];loadHandler&&loadHandler(1, '<div id="spr0_4384f0a4"><div id="spr1_4384f0a4" class="kern slide"><img id="img2_4384f0a4" src="data/img7.jpg" width="720px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_4384f0a4" class="kern slide"><div id="svg0_4384f0a4" style="left:70.735px;top:147.878px;"><svg width="419" height="33" viewBox="-1 -1 419 33"><path fill="#e4e1dd" stroke="#808080" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round" d="M1,1 h414.367 v28.653 h-414.367 Z"/></svg></div><div id="svg1_4384f0a4" style="left:-2px;top:-2px;"><svg width="724" height="64" viewBox="-1 -1 724 64"><path fill="#55160f" stroke="#002060" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round" d="M1,1 h720 v60 h-720 Z"/></svg></div><div id="spr3_4384f0a4" style="left:17.5px;top:4px;"><img id="img0_4384f0a4" src="data/img5.png" width="687" height="48.75" alt="Labs 8-13 and 4-6 Lab Report (per group)"/></div><div id="spr4_4384f0a4" style="left:7.75px;top:84px;"><img id="img1_4384f0a4" src="data/img6.png" width="700.5" height="445.5" alt="Make Word document or PowerPoint (no pdfs this time) titled: \
+Multiple Tube Fermentation Results Group (# )\
+\
+In the document, you should have:\
+an LTB result photo* from EACH group member\
+at least one BGLB result photo*\
+at least one EC result photo*\
+at least one EMB plate result photo*\
+*ALL photos should have in-photo labels (no labels added after the photo was taken)\
+\
+Send the document via CANVAS by the time we start lab on Wednesday, October 16th. \
+"/></div></div></div>');})();
