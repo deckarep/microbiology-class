12de66e329e6da101bdee2052d59ac0628d5462d
Ralph Caraveo <deckarep@gmail.com>
2019-11-19 20:12:00 -0800

    week 14,14b,15,15b,15c,15d

---
+++ docs/week15d/data/slide6.js
@@ -0,0 +1,17 @@
+(function(){var loadHandler=window['sl_{4887696A-4416-40B8-83EF-FBF68B603A4A}'];loadHandler&&loadHandler(5, '<div id="spr0_24c1d6f"><div id="spr1_24c1d6f" class="kern slide"><img id="img6_24c1d6f" src="data/img29.jpg" width="720px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_24c1d6f" class="kern slide"><div id="spr3_24c1d6f" style="left:93.655px;top:116.464px;"><img id="img0_24c1d6f" src="data/img25.jpg" width="532.5" height="411.75" alt="" style="left:0.071px;top:-0.047px;"/></div><div id="spr4_24c1d6f" style="left:32.5px;top:14.5px;"><img id="img1_24c1d6f" src="data/img26.png" width="658.5" height="95.25" alt="How to Make the Results Pages for Your Unknown Report"/></div><div id="spr5_24c1d6f" style="left:-3.5px;top:211.5px;"><img id="img2_24c1d6f" src="data/img30.png" width="237.75" height="310.5" alt="STEP 2: Include a GOOD-QUALITY, COLOR photo of your results. \
+Crop and/or enlarge photos as necessary; if I can’t see the results clearly, you get NO points."/></div><div id="spr6_24c1d6f" style="left:225px;top:111px;"><img id="img3_24c1d6f" src="data/img31.png" width="405.75" height="296.25" alt=""/></div><div id="svg7_24c1d6f" style="left:221.769px;top:233.337px;"><svg width="58" height="33" viewBox="-4.961 -5.563 58 33"><g fill="none"><g transform="translate(1.038895,2.814377)"><g fill="#ff0000" stroke="none" transform="translate(-1.038898,-2.814375)"><path stroke="#ff0000" stroke-width="6" stroke-linecap="butt" transform="translate(1.038898,2.814375)" d="M0,17.894 L33.394,5.567"/><path transform="matrix(0.938124,-0.3463,0.3463,0.938124,1.0389,20.708002)" d="M51.671,0 L33.81,8.93 V-8.93 Z"/></g></g></g></svg></div><div id="spr7_24c1d6f" style="left:327px;top:134.25px;"><img id="img4_24c1d6f" src="data/img36.png" width="398.25" height="378.75" alt=""/></div><div id="spr8_24c1d6f" style="left:340.5px;top:147px;"><img id="img5_24c1d6f" src="data/img37.png" width="372" height="351.75" alt="“GOOD-QUALITY, COLOR photo” means:\
+The photo IS NOT BLURRY.\
+\
+If the photo is of a stain, the ocular micrometer is in the picture, too.\
+\
+The results (color, precipitate, textures, etc.) are clearly visible.\
+\
+The photo is LARGE enough for the results to be seen.\
+\
+The photo is not squished, stretched, or distorted.\
+\
+Whatever you describe in the analysis section can be easily seen in the photo.\
+\
+\
+\
+"/></div></div></div>');})();
